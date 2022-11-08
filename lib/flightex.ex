5b19f8b279c8a6681d9f55d7a7689df9e045044d
Eric Kreis <erickreis793@gmail.com>
2022-11-08 01:31:39 -0300

    update: flightex & booking agent

--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,3 +1,14 @@
 defmodule Flightex do
+  alias Flightex.Bookings.Agent, as: BookinsAgent
+  alias Flightex.Bookings.CreateOrUpdate, as: CreateOrUpdateBookings
+  alias Flightex.Users.Agent, as: UserAgent
+  alias Flightex.Users.CreateOrUpdate, as: CreateOrUpdateUsers
 
+  def start_agents do
+    UserAgent.start_link(%{})
+    BookinsAgent.start_link(%{})
+  end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUsers, as: :call
+  defdelegate create_or_update_booking(params), to: CreateOrUpdateBookings, as: :call
 end
